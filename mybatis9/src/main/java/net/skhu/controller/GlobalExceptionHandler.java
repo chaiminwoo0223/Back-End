dc00dca913a82bc2eb40fc0a8e5816f00020f062
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-11-11 21:54:45 +0900

    Back-End

---
+++ mybatis9/src/main/java/net/skhu/controller/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package net.skhu.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String exceptionHandler(Exception exception) {
+        log.error("전역 에러", exception); // 에러를 로그에 기록
+        return "error";                  // 뷰(error.html)의 이름을 리턴
+    }
+}
