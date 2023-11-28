64f0ca68767a8ae15ec595a27a3dc7a5611f8824
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-11-28 15:54:39 +0900

    Back-End

---
+++ book7/src/test/java/net/skhu/Book7ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Book7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
