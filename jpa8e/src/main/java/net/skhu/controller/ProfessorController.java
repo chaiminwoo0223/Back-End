aa77f24575912b85a9b804bdc51cd936587bd6c1
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-12-05 13:05:58 +0900

    Back-End

---
+++ jpa8e/src/main/java/net/skhu/controller/ProfessorController.java
@@ -0,0 +1,96 @@
+package net.skhu.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import net.skhu.entity.Department;
+import net.skhu.entity.Professor;
+import net.skhu.model.Pagination;
+import net.skhu.model.ProfessorEdit;
+import net.skhu.service.DepartmentService;
+import net.skhu.service.ProfessorService;
+
+@Controller
+@RequestMapping("professor")
+public class ProfessorController {
+
+	@Autowired ProfessorService professorService;
+    @Autowired DepartmentService departmentService;
+
+    @GetMapping("list")
+    public String list(Model model, Pagination pagination, HttpServletRequest request) {
+    	pagination.setUrl(request.getRequestURL().toString());
+        List<Professor> professors = professorService.findAll(pagination);
+        model.addAttribute("professors", professors);
+        return "professor/list";
+    }
+
+    @GetMapping("create")
+    public String create(Model model, Pagination pagination) {
+        ProfessorEdit professorEdit = new ProfessorEdit();
+        List<Department> departments = departmentService.findAll();
+        model.addAttribute("professorEdit", professorEdit);
+        model.addAttribute("departments", departments);
+        return "professor/edit";
+    }
+
+    @PostMapping("create")
+    public String create(Model model, Pagination pagination,
+            @Valid ProfessorEdit professorEdit, BindingResult bindingResult) {
+    	try {
+    		professorService.insert(professorEdit, bindingResult, pagination);
+            return "redirect:list?" + pagination.getQueryString();
+        }
+        catch (Exception e) {
+            model.addAttribute("departments", departmentService.findAll());
+            bindingResult.rejectValue("", null, "등록할 수 없습니다.");
+            return "professor/edit";
+        }
+    }
+
+    @GetMapping("edit")
+    public String edit(Model model, int id, Pagination pagination) {
+    	ProfessorEdit professorEdit = professorService.findOne(id);
+        List<Department> departments = departmentService.findAll();
+        model.addAttribute("professorEdit", professorEdit);
+        model.addAttribute("departments", departments);
+        return "professor/edit";
+    }
+
+    @PostMapping(value="edit", params="cmd=save")
+    public String edit(Model model, Pagination pagination,
+            @Valid ProfessorEdit professorEdit, BindingResult bindingResult) {
+        try {
+        	professorService.update(professorEdit, bindingResult);
+            return "redirect:list?" + pagination.getQueryString();
+        }
+        catch (Exception e) {
+            model.addAttribute("departments", departmentService.findAll());
+            bindingResult.rejectValue("", null, "수정할 수 없습니다.");
+            return "professor/edit";
+        }
+    }
+
+    @PostMapping(value="edit", params="cmd=delete")
+    public String delete(Model model, Pagination pagination,
+            ProfessorEdit professorEdit, BindingResult bindingResult) {
+        try {
+        	professorService.delete(professorEdit.getId());
+            return "redirect:list?" + pagination.getQueryString();
+        }
+        catch (Exception e) {
+            model.addAttribute("departments", departmentService.findAll());
+            bindingResult.rejectValue("", null, "삭제할 수 없습니다.");
+            return "professor/edit";
+        }
+    }
+}
