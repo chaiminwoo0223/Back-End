ac497889800ce291ad88773f44ffae7ed8822cfe
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-12-07 16:54:21 +0900

    Back-End

---
+++ movie/src/main/java/net/skhu/E202114058Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E202114058Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E202114058Application.class, args);
+	}
+
+}
